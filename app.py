70af28e6dcbf5daa9001eaee2ed001fed4d8eb8e
Abhay Singh <abhaysinghbishen@gmail.com>
2024-08-09 17:16:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ import ipaddress
 import threading
 import paramiko
 import json
+import logging
 
 app = Flask(__name__)
+
+# Acquire the logger for a library (azure.mgmt.resource in this example)
+logger = logging.getLogger('azure.mgmt.resource')
+logger.setLevel(logging.DEBUG)
 
 # Replace these values with your own
 workspace_id = "subscriptions/3846cb0f-4afa-47ee-8ea4-1c8449c8c8d9/resourcegroups/functionapptostorageflow/providers/microsoft.operationalinsights/workspaces/nsplog"
@@ -49,7 +54,7 @@ def is_ip_in_prefix(ip_str, prefix_str):
 
 def periodic_refresh_service_tags_cache_nmagent_api():
     # Your task logic here
-    print("periodic_refresh_service_tags_cache_nmagent_api")
+    logger.debug("periodic_refresh_service_tags_cache_nmagent_api")
     refresh_service_tags_cache_nmagent_api()
 
     # Schedule the next call
@@ -57,7 +62,7 @@ def periodic_refresh_service_tags_cache_nmagent_api():
 
 def refresh_service_tags_cache_nmagent_api():
 # VM credentials
-    hostname = '10.0.1.4' #Public IP of VM: 20.228.36.129
+    hostname = '20.228.36.129' #Public IP of VM: 20.228.36.129
     port = 22
     username = 'testAdmin'
     passw = 'testPassword@1'
@@ -81,9 +86,9 @@ def refresh_service_tags_cache_nmagent_api():
         service_tags_json = json.loads(stdout_output)
     except json.JSONDecodeError as e:
         # Handle the case where `stdout_output` is not valid JSON
-        print(f"Error decoding JSON: {e}")
+        logger.debug(f"Error decoding JSON: {e}")
         # Optionally print the raw output for debugging
-        print("Raw output:", stdout_output)
+        logger.debug("Raw output:", stdout_output)
         return
     
     version = service_tags_json['version']
@@ -93,11 +98,11 @@ def refresh_service_tags_cache_nmagent_api():
         service_tags_map[systemTag['name']] = systemTag['ipV4']
         service_tags_map[systemTag['name']].extend(systemTag['ipV6'])
 
-    print("Metering File Version: ", version, "Total in discovery:", len(systemTags), "Total in dict:", len(service_tags_map))
+    logger.debug("Metering File Version: ", version, "Total in discovery:", len(systemTags), "Total in dict:", len(service_tags_map))
 
 def periodic_refresh_service_tags_cache_discovery_api():
     # Your task logic here
-    print("periodic_refresh_service_tags_cache_discovery_api")
+    logger.debug("periodic_refresh_service_tags_cache_discovery_api")
     refresh_service_tags_cache_discovery_api()
 
     # Schedule the next call
@@ -109,7 +114,7 @@ def refresh_service_tags_cache_discovery_api():
 
     for value in values:
         service_tags_map[value['name']] = value['properties']['addressPrefixes']
-    print("Total in discovery:", len(values), "Total in dict:", len(service_tags_map))
+    logger.debug("Total in discovery:", len(values), "Total in dict:", len(service_tags_map))
 
 def process_la_result(json_result):    
     # Extract column names and rows from the JSON data
@@ -155,7 +160,7 @@ def query_log_analytics():
 
     if response.status_code == 200:
         suggestions = process_la_result(response.json())
-        print(suggestions)
+        logger.debug(suggestions)
         return jsonify(suggestions)
     else:
         return jsonify({'error': response.text}), response.status_code
